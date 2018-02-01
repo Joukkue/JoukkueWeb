c34c9b9a198085b3248b2bb58c3c12b2127c0917
Westblat <joonas.malinen@hotmail.fi>
2018-02-01 17:09:01 +0000

    Added the basic files for django

---
+++ JoukkuePage/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for JoukkuePage project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+sys.path.append("/home/pi/djangoenv/lib/python3.5/site-packages")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "JoukkuePage.settings")
+
+
+from django.core.wsgi import get_wsgi_application
+
+
+path = '/home/pi/JoukkuePage/JoukkuePage'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+os.environ["DJANGO_SETTINGS_MODULE"] = "JoukkuePage.settings"
+application = get_wsgi_application()
