9f10e3e947d2bea04cf0a66e2318434069884d2f
Westblat <joonas.malinen@hotmail.fi>
2018-02-28 16:51:11 +0000

    Fixes for linux side

--- JoukkuePage/settings.py
+++ JoukkuePage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*bdx!&3^y7(kl2f9sjxbh-jozn$uus*+q38-vt)&@2q+xbt1sl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["192.168.0.100"]
 
 
 # Application definition
--- WebCode/myviews/front.py
+++ WebCode/myviews/front.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 import sqlite3
+from sys import platform
 
 def frontpage(request):
     return render(request, "home.html")
 
 def show(request):
-    connection = sqlite3.connect('joukkue.db')
+    if platform == "linux":
+        connection = sqlite3.connect('/home/pi/JoukkueBot/joukkue.db')
+    else:
+        connection = sqlite3.connect('joukkue.db')
+        #connection = sqlite3.connect('/home/pi/JoukkueBot/joukkue.db')
     c = connection.cursor()
     c.execute('SELECT * FROM Quotes')
     quotes = c.fetchall()
@@ -13,5 +18,5 @@ def show(request):
     context['quotes'] = {}
     for quote in quotes:
         context['quotes'][quote[0]] = quote[1]
-    print (context)
+    
     return render(request, "quotes.html", context)
